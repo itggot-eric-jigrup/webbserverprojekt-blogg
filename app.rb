b35f4e601272cf064185b78484c2cceaccbe8875
itggot-eric-jigrup <eric.jigrup@itggot.se>
2019-02-21 10:19:16 +0100

    .

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'slim'
 require 'SQLite3'
+require 'bcrypt'
 
     get('/') do
-        slim(:start)
+        slim(:index)
     end
 
     get('/login') do
@@ -11,4 +12,27 @@ require 'SQLite3'
     end 
 
     post('/login') do
+        db = SQLite3::Database.new("db/user.db")
+        db.result_as_hash = true
+
+        if BCrypt::Password.new == password
+            redirect('/inloggad')
+        else
+            redirect('/login')
+        end
     end 
+
+    get('/registrering') do
+        slim(:registrering)
+    end
+
+    post('/registrering') do 
+        db = SQLite3::Database.new("db/user.db")
+        db.result_as_hash = true
+
+        name = params["name"]
+        password = BCrypt::Password.create(params["password"])
+
+        db.execute("INSERT INTO user (name,password) VALUES (?,?)",name,password)
+        redirect('/') 
+    end
